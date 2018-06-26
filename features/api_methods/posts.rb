778f3c145644ba7e17f3935549259c279a31060d
Modris Ozols <modris.ozols@testdevlab.com>
2018-06-26 16:46:54 +0300

    3. Uzdevums

---
+++ features/api_methods/posts.rb
@@ -0,0 +1,35 @@
+def get_random_users_post(user:, post:)
+  body = {
+    email: user.email,
+    password: user.password
+  }.to_json
+
+  response = API.get('http://195.13.194.180:8090/api/posts',
+                      headers: { authorization: user.auth_token })
+
+  # Check if response 200 OK
+  assert_status_code(200, response, 'Get All Posts')
+  resp = JSON.parse(response)
+  post_data = resp.reject!{|post| post["author"]["id"] == user.id}.sample
+  post.id = post_data["id"]
+  post.author = post_data["author"]["id"]
+  post.title = post_data["title"]
+  post.content = post_data["content"]
+  post.created_at = post_data["createdAt"]
+  post.updated_at = post_data["updatedAt"]
+end
+
+def edit_post_title_negative(user:, post:)
+  payload = {
+    title: "#{post.title} + Changed + #{user.firstname}}"
+  }.to_json
+  response = API.put("http://195.13.194.180:8090/api/post?post_id=#{post.id}",
+                      headers: { authorization: user.auth_token },
+                      payload: payload)
+
+  resp = JSON.parse(response)
+  assert_status_code(403, response, 'Post updated')
+  resp = JSON.parse(response)
+  assert_equal('Forbidden', resp['error'], "Post title doesn't match")
+  assert_equal('You cannot delete a post that has not been created by you!', resp['message'], "Post title doesn't match")
+end
---
+++ features/domain_objects/post.rb
@@ -0,0 +1,3 @@
+class Post
+  attr_accessor :id, :author, :title, :content, :created_at, :updated_at
+end
